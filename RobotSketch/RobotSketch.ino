b0c34998c4747a6aaa6ee4156de9de8e2e12672d
dnsomoano <dnsomoano@gmail.com>
2020-02-23 21:35:25 -0500

    Compiled and uploaded, but no activity from h bridge.

--- RobotSketch/RobotSketch.ino
+++ RobotSketch/RobotSketch.ino
@@ -1,12 +1,13 @@
+// H-Bridge Pins
 int EN_A = 10; // Enable pin for first motor
-int IN1 = 9; // Control pin for first motor
-int IN2 = 8; // Control pin for first motor
-//int IN3 = 7; // Control pin for second motor 
-//int IN4 = 6; // Control pin for second motor 
-//int EN_B = 5; // Enable pin for second motor
+int IN1 = 9; // Control pin for first motor(for direction)
+int IN2 = 8; // Control pin for first motor(for direction)
+int IN3 = 7; // Control pin for second motor 
+int IN4 = 6; // Control pin for second motor 
+int EN_B = 5; // Enable pin for second motor
 
 // Variables for motors
-int motor_speed1 = 0;
+int motor_speed1 = 255;
 int motor_speed2 = 0;
 
 void setup()
@@ -16,9 +17,9 @@ void setup()
     pinMode(EN_A, OUTPUT);
     pinMode(IN1, OUTPUT);  
     pinMode(IN2, OUTPUT);
-    //pinMode(IN3, OUTPUT);  
-    //pinMode(IN4, OUTPUT);
-    //pinMode(EN_B, OUTPUT);
+    pinMode(IN3, OUTPUT);  
+    pinMode(IN4, OUTPUT);
+    pinMode(EN_B, OUTPUT);
 }
 
 void loop()
@@ -29,8 +30,9 @@ void loop()
     Serial.println(IR_SENSOR1);
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
-    analogWrite(EN_A, 100);
+    delay(25);
+    analogWrite(EN_A, motor_speed1);
     //delay(3000);
-    analogWrite(EN_A, 0);
+    //analogWrite(EN_A, 0);
     delay(3000);
 }
